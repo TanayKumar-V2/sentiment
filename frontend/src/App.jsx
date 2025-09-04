bd63a5500f6b7eea8b90cc3454e82d4d2b32d067
TanayKumar-V2 <tanayk726@gmail.com>
2025-09-04 13:24:03 +0530

    "fix bro"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -50,7 +50,7 @@ function App() {
     const payload = inputType === 'text' ? { text: inputValue } : { url: inputValue };
 
     try {
-      const response = await fetch(`https://sentiment-3x9d.onrender.com${endpoint}`, {
+      const response = await fetch(`https://sentiment-1-5lof.onrender.com${endpoint}`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload),
